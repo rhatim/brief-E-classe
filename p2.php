72d3c0ea0f4adf81a5965dfeb75fa55c7fe9c49a
hatim <rouchdi.hatim@gmail.com>
2022-01-19 22:55:52 +0100

    include-php

---
+++ p2.php
@@ -0,0 +1,46 @@
+<?php
+include 'all.php'
+?>
+
+
+            <div class="row mt-5 mx-2">
+                <div class="col-lg col-md-6 mb-3">
+                    <div class="h-100 px-2 py-1" style="background-color: #F0F9FF; border-radius: 8px;">
+                        <h1><i class="bi bi-mortarboard" style="color: #74C1ED;"></i></h1>
+                        <p>Students</p>
+                        <h3 class="float-end">243</h3>
+                    </div>
+                </div>
+                <div class="col-lg col-md-6 mb-3">
+                    <div class="h-100 px-2 py-1" style="background-color: #FEF6FB; border-radius: 8px;">
+                        <h1><i class="bi bi-bookmark" style="color: #EE95C5;"></i></h1>
+                        <p>Course</p>
+                        <h3 class="float-end">13</h3>
+                    </div>
+                </div>
+                <div class="col-lg col-md-6 mb-3">
+                    <div class="h-100 px-2 py-2" style="background-color: #FEFBEC; border-radius: 8px;">
+                        <h3><i class="bi bi-currency-dollar" style="color: #00C1FE; border: 2px solid #00C1FE;"></i></h3>
+                        <p>Payments</p>
+                        <div class="d-lg-flex align-items-center float-end">
+                            <h6>DHS</h6><h3>556,000</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg  col-md-6 mb-3">
+                    <div class="h-100 px-2 py-1" style="background-image: linear-gradient(120deg,#00C1FE,#FAFFC1); border-radius: 8px;">
+                        <h1><i class="bi bi-person text-light"></i></h1>
+                        <p class="text-light">User</p>
+                        <h3 class="float-end">3</h3>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ pay.php
@@ -0,0 +1,79 @@
+<?php
+include 'all.php'
+?>
+            <div>
+              <div class="row mt-4">
+                <div class="col-md d-flex justify-content-between">
+                    <div class="">
+                        <h3>Payment Details</h3>
+                    </div>
+                    <div class="">
+                        <img class="" src="images/scroll.svg" alt="">
+                    </div>
+                </div>
+            </div>  
+            <hr>
+            <div style="overflow: auto;" class="row">
+                <table class="table table-hover table-striped">
+                    <thead style="color: rgba(78, 73, 73, 0.211); " class="bg-light ">
+                      <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Payment Schedule</th>
+                        <th scope="col">Bill Number</th>
+                        <th scope="col">Amount Paid</th>
+                        <th scope="col">Balance amountr</th>
+                        <th scope="col">Date</th>
+                        <th scope="col"> </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Karthi</td>
+                        <td>First</td>
+                        <td>00012223</td>
+                        <td>DHS 100,000</td>
+                        <td>DHS 500,000</td>
+                        <td>05-Jan, 2022</td>
+                        <td><i class="bi bi-eye text-info"></i></td>
+                      </tr>
+                      <tr>
+                        <td>Karthi</td>
+                        <td>First</td>
+                        <td>00012223</td>
+                        <td>DHS 100,000</td>
+                        <td>DHS 500,000</td>
+                        <td>05-Jan, 2022</td>
+                        <td><i class="bi bi-eye text-info"></i></td>
+                      </tr>
+                      <tr>
+                        <td>Karthi</td>
+                        <td>First</td>
+                        <td>00012223</td>
+                        <td>DHS 100,000</td>
+                        <td>DHS 500,000</td>
+                        <td>05-Jan, 2022</td>
+                        <td><i class="bi bi-eye text-info"></i></td>
+                      </tr>
+                      <tr>
+                        <td>Karthi</td>
+                        <td>First</td>
+                        <td>00012223</td>
+                        <td>DHS 100,000</td>
+                        <td>DHS 500,000</td>
+                        <td>05-Jan, 2022</td>
+                        <td><i class="bi bi-eye text-info"></i></td>
+                      </tr>
+
+                     
+                     
+                     
+                      
+                    </tbody>
+                  </table>
+                </div>
+            </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
