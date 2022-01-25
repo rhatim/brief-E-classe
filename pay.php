0276031210e16b05b072fc37c575dc222e338efd
hatim <rouchdi.hatim@gmail.com>
2022-01-25 15:07:22 +0100

    width

--- pay.php
+++ pay.php
@@ -1,88 +1,101 @@
 <?php
 include 'all.php'
 ?>
-            <div>
-              <div class="row mt-4">
-                <div class="col-md d-flex justify-content-between">
-                    <div class="">
-                        <h3>Payment Details</h3>
-                    </div>
-                    <div class="">
-                        <img class="" src="images/scroll.svg" alt="">
-                    </div>
-                </div>
-            </div>  
-            <hr>
-            <div style="overflow: auto;" class="row">
-                <table class="table table-hover table-striped">
-                    <thead style="color: rgba(78, 73, 73, 0.211); " class="bg-light ">
-                      <tr>
-                        <th scope="col">Name</th>
-                        <th scope="col">Payment Schedule</th>
-                        <th scope="col">Bill Number</th>
-                        <th scope="col">Amount Paid</th>
-                        <th scope="col">Balance amountr</th>
-                        <th scope="col">Date</th>
-                        <th scope="col"> </th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                    <?php 
-                    $payments = [
-                      [
-                        'name' => 'karthi',
-                        'payment_schedule' => 'first',
-                        'bill-Number' => '00012223',
-                        'amount_paid' => 'dhs_100,000',
-                        'balance_amount' => 'dhs_500,000',
-                        'date' => '05-jan,_2022',
-                      ],
-                      [
-                        'name' => 'karthi',
-                        'payment_schedule' => 'first',
-                        'bill-Number' => '00012223',
-                        'amount_paid' => 'dhs_100,000',
-                        'balance_amount' => 'dhs_500,000',
-                        'date' => '05-jan,_2022',
-                      ],[
-                        'name' => 'karthi',
-                        'payment_schedule' => 'first',
-                        'bill-Number' => '00012223',
-                        'amount_paid' => 'dhs_100,000',
-                        'balance_amount' => 'dhs_500,000',
-                        'date' => '05-jan,_2022',
-                      ],[
-                        'name' => 'karthi',
-                        'payment_schedule' => 'first',
-                        'bill-Number' => '00012233',
-                        'amount_paid' => 'dhs_100,000',
-                        'balance_amount' => 'dhs_500,000',
-                        'date' => '05-jan,_2022',
-                      ]
-                      ];
-                      foreach($payments as $user){
-                        echo "<tr>
-                          <td>".$user['name']."</td>
-                          <td>".$user['payment_schedule']."</td>
-                          <td>".$user['bill-Number']."</td>
-                          <td>".$user['amount_paid']."</td>
-                          <td>".$user['balance_amount']."</td>
-                          <td>".$user['date']."</td>
-                          <td>".$user['name']."</td>
+<div class="mx-3">
+  <div class="row mt-4">
+    <div class="col-md d-flex justify-content-between">
+      <div class="">
+        <h3>Payment Details</h3>
+      </div>
+      <div class="">
+        <img class="" src="images/scroll.svg" alt="">
+      </div>
+    </div>
+  </div>
+  <hr>
+  <div style="overflow: auto;" class="row">
+    <table class="table table-hover table-borderless table-striped px-2">
+      <thead style="color: rgba(78, 73, 73, 0.211); " class="bg-light mx-1">
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Payment Schedule</th>
+          <th scope="col">Bill Number</th>
+          <th scope="col">Amount Paid</th>
+          <th scope="col">Balance amountr</th>
+          <th scope="col">Date</th>
+          <th scope="col"> </th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        $payments = [
+          [
+            'name' => 'karthi',
+            'payment_schedule' => 'first',
+            'bill-Number' => '00012223',
+            'amount_paid' => 'dhs_100,000',
+            'balance_amount' => 'dhs_500,000',
+            'date' => '05-jan,_2022',
+          ],
+          [
+            'name' => 'karthi',
+            'payment_schedule' => 'first',
+            'bill-Number' => '00012223',
+            'amount_paid' => 'dhs_100,000',
+            'balance_amount' => 'dhs_500,000',
+            'date' => '05-jan,_2022',
+          ], [
+            'name' => 'karthi',
+            'payment_schedule' => 'first',
+            'bill-Number' => '00012223',
+            'amount_paid' => 'dhs_100,000',
+            'balance_amount' => 'dhs_500,000',
+            'date' => '05-jan,_2022',
+          ], [
+            'name' => 'karthi',
+            'payment_schedule' => 'first',
+            'bill-Number' => '00012233',
+            'amount_paid' => 'dhs_100,000',
+            'balance_amount' => 'dhs_500,000',
+            'date' => '05-jan,_2022',
+          ]
+        ];
+        echo "<tr class='d-none'>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      
+
+                    
+                    </tr>";
+        foreach ($payments as $user) {
+
+          echo "<tr>
+                          <td>" . $user['name'] . "</td>
+                          <td>" . $user['payment_schedule'] . "</td>
+                          <td>" . $user['bill-Number'] . "</td>
+                          <td>" . $user['amount_paid'] . "</td>
+                          <td>" . $user['balance_amount'] . "</td>
+                          <td class='text-nowrap'>" . $user['date'] . "</td>
                           <td><i class='bi bi-eye text-info'></i></td>
                           
 
                         
                         </tr>";
-                      }
-                                    
-                                ?>
-                    </tbody>
-                  </table>
-                </div>
-            </div>
+        }
+
+        ?>
+      </tbody>
+    </table>
+  </div>
+</div>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
+
 </html>
--- students.php
+++ students.php
@@ -1,125 +1,129 @@
 <?php
 include 'all.php'
 ?>
-            <div>
-              <div class="row mt-4">
-                <div class="col-md d-flex justify-content-between">
-                    <div class="">
-                        <h3>Students List</h3>
-                    </div>
-                    <div class="">
-                        <img class="" src="images/scroll.svg" alt="">
-                        <button class="btn btn-info text-white ">ADD NEW STUDENT</button> 
-                    </div>  
-                </div>
-              </div>
-              <hr>
-              <div style="overflow: auto;" class="row">
-                <table>
-                    <thead style="color: rgba(78, 73, 73, 0.211); " class="bg-light ">
-                      <tr>
-                        <th scope="col"></th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Phone </th>
-                        <th scope="col">Enroll Number</th>
-                        <th scope="col">Date of admission</th>
-                        <th scope="col"> </th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <?php
-                      $students = [
-                        [
-                          'name' => 'username',
-                          'email' => 'user@email.com',
-                          'phone' => '7305477760',
-                          'enroll_number' => '1234567305477760',
-                          'date_of_admission' => '08,-dec, 2021',
-                          'email' => 'user@email.com',
+<div>
+  <div class="row mt-4">
+    <div class="col-md d-flex justify-content-between ms-2">
+      <div class="">
+        <h3>Students List</h3>
+      </div>
+      <div class="me-3">
+        <img class="" src="images/scroll.svg" alt="">
+        <button class="btn btn-info text-white ">ADD NEW STUDENT</button>
+      </div>
+    </div>
+  </div>
+  <hr>
+  <div class="conatiner-fluid mx-auto" >
+    <div class="row mx-2" style="overflow-x: auto;">
+      <table>
+        <thead style="color: rgba(78, 73, 73, 0.211); " class="bg-light">
+          <tr>
+            <th scope="col"></th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone </th>
+            <th scope="col">Enroll Number</th>
+            <th class="text-nowrap" scope="col">Date of admission</th>
+            <th scope="col"> </th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          $students = [
+            [
+              'name' => 'username',
+              'email' => 'user@email.com',
+              'phone' => '7305477760',
+              'enroll_number' => '1234567305477760',
+              'date_of_admission' => '08,-dec, 2021',
+              'email' => 'user@email.com',
 
-                        ],
-                        [
-                          'name' => 'username',
-                          'email' => 'user@email.com',
-                          'phone' => '7305477760',
-                          'enroll_number' => '1234567305477760',
-                          'date_of_admission' => '08,-dec, 2021',
-                          'email' => 'user@email.com',
+            ],
+            [
+              'name' => 'username',
+              'email' => 'user@email.com',
+              'phone' => '7305477760',
+              'enroll_number' => '1234567305477760',
+              'date_of_admission' => '08,-dec, 2021',
+              'email' => 'user@email.com',
 
-                        ],
-                        [
-                          'name' => 'username',
-                          'email' => 'user@email.com',
-                          'phone' => '7305477760',
-                          'enroll_number' => '1234567305477760',
-                          'date_of_admission' => '08,-dec, 2021',
-                          'email' => 'user@email.com',
+            ],
+            [
+              'name' => 'username',
+              'email' => 'user@email.com',
+              'phone' => '7305477760',
+              'enroll_number' => '1234567305477760',
+              'date_of_admission' => '08,-dec, 2021',
+              'email' => 'user@email.com',
 
-                        ],
-                        [
-                          'name' => 'username',
-                          'email' => 'user@email.com',
-                          'phone' => '7305477760',
-                          'enroll_number' => '1234567305477760',
-                          'date_of_admission' => '08,-dec, 2021',
-                          'email' => 'user@email.com',
+            ],
+            [
+              'name' => 'username',
+              'email' => 'user@email.com',
+              'phone' => '7305477760',
+              'enroll_number' => '1234567305477760',
+              'date_of_admission' => '08,-dec, 2021',
+              'email' => 'user@email.com',
 
-                        ],
-                        [
-                          'name' => 'username',
-                          'email' => 'user@email.com',
-                          'phone' => '7305477760',
-                          'enroll_number' => '1234567305477760',
-                          'date_of_admission' => '08,-dec, 2021',
-                          'email' => 'user@email.com',
+            ],
+            [
+              'name' => 'username',
+              'email' => 'user@email.com',
+              'phone' => '7305477760',
+              'enroll_number' => '1234567305477760',
+              'date_of_admission' => '08,-dec, 2021',
+              'email' => 'user@email.com',
 
-                        ],
-                        [
-                          'name' => 'username',
-                          'email' => 'user@email.com',
-                          'phone' => '7305477760',
-                          'enroll_number' => '1234567305477760',
-                          'date_of_admission' => '08,-dec, 2021',
-                          'email' => 'user@email.com',
+            ],
+            [
+              'name' => 'username',
+              'email' => 'user@email.com',
+              'phone' => '7305477760',
+              'enroll_number' => '1234567305477760',
+              'date_of_admission' => '08,-dec, 2021',
+              'email' => 'user@email.com',
 
-                        ],
-                        [
-                          'name' => 'username',
-                          'email' => 'user@email.com',
-                          'phone' => '7305477760',
-                          'enroll_number' => '1234567305477760',
-                          'date_of_admission' => '08,-dec, 2021',
-                          'email' => 'user@email.com',
+            ],
+            [
+              'name' => 'username',
+              'email' => 'user@email.com',
+              'phone' => '7305477760',
+              'enroll_number' => '1234567305477760',
+              'date_of_admission' => '08,-dec, 2021',
+              'email' => 'user@email.com',
 
-                        ]
-                        ];
-                        
-                        foreach ($students as $stdnt){
-                          echo "<tr>
+            ]
+          ];
+
+          foreach ($students as $stdnt) {
+            echo "<tr>
                                <td> <img src='images/table.svg' alt=></td>
-                               <td>".$stdnt['name']."</td>
-                               <td>".$stdnt['email']."</td>
-                               <td>".$stdnt['phone']."</td>
-                               <td>".$stdnt['enroll_number']."</td>
-                               <td>".$stdnt['date_of_admission']."</td>
+                               <td>" . $stdnt['name'] . "</td>
+                               <td>" . $stdnt['email'] . "</td>
+                               <td>" . $stdnt['phone'] . "</td>
+                               <td>" . $stdnt['enroll_number'] . "</td>
+                               <td class='text-nowrap'>" . $stdnt['date_of_admission'] . "</td>
                                
-                               <td><i class='bi bi-pencil text-info mx-4'></i><i class='bi bi-trash text-info'></i></td>
+                               <td class='text-nowrap'><i class='bi bi-pencil text-info mx-4'></i></td>
+
+                               <td class='text-nowrap'><i class='bi bi-trash text-info'></i></td>
                           </tr>";
-
-                        }
-
-
-                      ?>
-                      
-
-                   
+          }
 
 
-                     </tbody>
-                  </table>
-              </div>
-            </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-                            </body>
-                            </html>
+          ?>
+
+
+
+
+
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
